b31f5c1264ace06e53bc5ff774207aacde069497
ExtraordinaryBen <ben@powerpup.yi.org>
2012-04-15 00:09:13 -0700

    Updated for 1.2.x

--- src/net/mcbat/doorsopendoors/DoorsOpenDoors.java
+++ src/net/mcbat/doorsopendoors/DoorsOpenDoors.java
@@ -1,10 +1,9 @@
 package net.mcbat.doorsopendoors;
 
 import java.util.logging.Logger;
-
+import org.bukkit.plugin.java.JavaPlugin;
 import net.mcbat.doorsopendoors.listeners.DoorsOpenDoorsPlayerListener;
 
-import org.bukkit.plugin.java.JavaPlugin;
 
 public class DoorsOpenDoors extends JavaPlugin {
 	private final Logger _logger = Logger.getLogger("minecraft");
@@ -12,10 +11,10 @@ public class DoorsOpenDoors extends JavaPlugin {
 	@Override
 	public void onEnable() {
 		_logger.info("[Doors Open Doors] v"+this.getDescription().getVersion()+" (Helium) enabled.");
-		_logger.info("[Doors Open Doors] Developed by: [Mattera, Steven (IchigoKyger)].");
+		_logger.info("[Doors Open Doors] Developed by: [Mattera, Steven (IchigoKyger), Updated for 1.2 by ExtraordinaryBen].");
 		_logger.info("[Doors Open Doors] Based on: Kassoon's single player mod.");
 
-		(new DoorsOpenDoorsPlayerListener(this)).registerEvents();
+		new DoorsOpenDoorsPlayerListener(this);
 	}
 	
 	@Override
--- src/net/mcbat/doorsopendoors/listeners/DoorsOpenDoorsPlayerListener.java
+++ src/net/mcbat/doorsopendoors/listeners/DoorsOpenDoorsPlayerListener.java
@@ -5,90 +5,89 @@ import net.mcbat.doorsopendoors.DoorsOpenDoors;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
-import org.bukkit.event.Event;
-import org.bukkit.event.Event.Priority;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractEvent;
-import org.bukkit.event.player.PlayerListener;
 
-public class DoorsOpenDoorsPlayerListener extends PlayerListener {
-	private final DoorsOpenDoors _plugin;
-	
+
+
+public class DoorsOpenDoorsPlayerListener implements Listener {
+
 	public DoorsOpenDoorsPlayerListener(DoorsOpenDoors plugin) {
-		_plugin = plugin;
+		plugin.getServer().getPluginManager().registerEvents(this, plugin);
+
 	}
 	
-	public void registerEvents() {
-		_plugin.getServer().getPluginManager().registerEvent(Event.Type.PLAYER_INTERACT, this, Priority.Highest, _plugin);
-	}
 	
+	@EventHandler(priority = EventPriority.HIGHEST)
 	public void onPlayerInteract(PlayerInteractEvent event) {
 		if (event.isCancelled())
 			return;
 		
 		if (event.getClickedBlock().getType() == Material.WOODEN_DOOR) {
-			Block doorBlock = event.getClickedBlock();
-			Block otherDoorTopBlock = null;
-			Block otherDoorBottomBlock = null;
-
-			if (this.doorIsTopHalf(doorBlock.getData()))
-				doorBlock = doorBlock.getRelative(BlockFace.DOWN);
 			
-			if (doorBlock.getRelative(BlockFace.NORTH).getType() == Material.WOODEN_DOOR && doorIsConnected(doorBlock.getData(), doorBlock.getRelative(BlockFace.NORTH).getData())) {
-				otherDoorBottomBlock = doorBlock.getRelative(BlockFace.NORTH);
-			}
-			else if (doorBlock.getRelative(BlockFace.SOUTH).getType() == Material.WOODEN_DOOR && doorIsConnected(doorBlock.getData(), doorBlock.getRelative(BlockFace.SOUTH).getData())) {
-				otherDoorBottomBlock = doorBlock.getRelative(BlockFace.SOUTH);
-			}
-			else if (doorBlock.getRelative(BlockFace.EAST).getType() == Material.WOODEN_DOOR && doorIsConnected(doorBlock.getData(), doorBlock.getRelative(BlockFace.EAST).getData())) {
-				otherDoorBottomBlock = doorBlock.getRelative(BlockFace.EAST);
-			}
-			else if (doorBlock.getRelative(BlockFace.WEST).getType() == Material.WOODEN_DOOR && doorIsConnected(doorBlock.getData(), doorBlock.getRelative(BlockFace.WEST).getData())) {
-				otherDoorBottomBlock = doorBlock.getRelative(BlockFace.WEST);
-			}
+			Block doorBlockClicked = event.getClickedBlock();
+			Block door1Top = null;
+			Block door1Bottom = null;
+			
+			Block door2Bottom = null;
 
-			if (otherDoorBottomBlock != null) {
-				otherDoorTopBlock = otherDoorBottomBlock.getRelative(BlockFace.UP);
+			if ( doorIsTopHalf(doorBlockClicked.getData()) ) {
+				door1Top = doorBlockClicked;
+				door1Bottom = doorBlockClicked.getRelative(BlockFace.DOWN);
+			}
+			else {
+				door1Top = doorBlockClicked.getRelative(BlockFace.UP);
+				door1Bottom = doorBlockClicked;
+			}
+			
+			
+			if (door1Bottom.getRelative(BlockFace.NORTH).getType() == Material.WOODEN_DOOR && doorIsConnected(door1Top.getData(), door1Top.getRelative(BlockFace.NORTH).getData())) {
+				door2Bottom = door1Bottom.getRelative(BlockFace.NORTH);
+			}
+			else if (door1Bottom.getRelative(BlockFace.SOUTH).getType() == Material.WOODEN_DOOR && doorIsConnected(door1Top.getData(), door1Top.getRelative(BlockFace.SOUTH).getData())) {
+				door2Bottom = door1Bottom.getRelative(BlockFace.SOUTH);
+			}
+			else if (door1Bottom.getRelative(BlockFace.EAST).getType() == Material.WOODEN_DOOR && doorIsConnected(door1Top.getData(), door1Top.getRelative(BlockFace.EAST).getData())) {
+				door2Bottom = door1Bottom.getRelative(BlockFace.EAST);
+			}
+			else if (door1Bottom.getRelative(BlockFace.WEST).getType() == Material.WOODEN_DOOR && doorIsConnected(door1Top.getData(), door1Top.getRelative(BlockFace.WEST).getData())) {
+				door2Bottom = door1Bottom.getRelative(BlockFace.WEST);
+			}
+			
+
+			if (door2Bottom != null) {
 				
-				if (this.doorIsOpen(doorBlock.getData()) != this.doorIsOpen(otherDoorTopBlock.getData())) {
-					otherDoorTopBlock.setData(flipDoor(otherDoorTopBlock.getData()));
-					otherDoorBottomBlock.setData(flipDoor(otherDoorBottomBlock.getData()));
+				if ( doorIsOpen(door1Bottom.getData()) == doorIsOpen(door2Bottom.getData())) {
+					door2Bottom.setData(flipDoor(door2Bottom.getData()));
 				}
 			}
 		}
 	}
 	
-	private boolean doorIsConnected(byte data, byte data2) {
-		int f1 = -1, f2 = -1, h1 = -1, h2 = -1;
-
-		f1 = (data & 0x3);
-		f2 = (data2 & 0x3);
-		
-		if ((data & 0x3) == 0x3)		h1 = 3;
-		else if ((data & 0x1) == 0x1)	h1 = 1;
-		else if ((data & 0x2) == 0x2)	h1 = 2;
-		else							h1 = 0;
-
-		if ((data2 & 0x3) == 0x3)		h2 = 3;
-		else if ((data2 & 0x1) == 0x1)	h2 = 1;
-		else if ((data2 & 0x2) == 0x2)	h2 = 2;
-		else							h2 = 0;
-		
-		
-		if (f1 == f2 && f2 == h1 && h1 == h2)
+	private boolean doorIsConnected(byte door1Top, byte door2Top) {
+		if ((door1Top & 0x1) == (door2Top & 0x1)) //If hinges are both the same side.
 			return false;
-		
-		if (f1 == h1 && f2 == h2)
+		else 
 			return true;
 		
-		return false;
 	}
 	
 	private boolean doorIsTopHalf(byte data) {
-		return ((data & 0x8) == 0x8);
+		if((data & 0x8) == 0x08)
+			return true;
+		else 
+			return false;
+
 	}
 	
 	private boolean doorIsOpen(byte data) {
-		return ((data & 0x4) == 0x4);
+		if((data & 0x4) == 0x4) 
+			return true;
+		else 
+			return false;
+
 	}
 	
 	private byte flipDoor(byte data) {
